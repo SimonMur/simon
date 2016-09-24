661f3d76645b035aab0c3dc3bde6807202fe311d
unknown <symonmuriuki411@gmail.com>
2016-09-24 10:52:39 +0300

    improve

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
      if params[:tag]
        @posts = Post.tagged_with(params[:tag])
      else
-       @posts = Post.all
+      @posts = Post.all.paginate(page: params[:page], per_page: 4)
      end
  end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,11 @@ class Post < ActiveRecord::Base
 	default_scope -> { order(created_at: :desc) }
 	mount_uploader :picture, PictureUploader
 	acts_as_taggable_on :tags
-	
+	def self.tag_post_list(page, tag)
+
+  @posts = Post.tagged_with(params[:id]).paginate(:page => params[:page], :per_page => 4)
+
+end
 	extend FriendlyId
     friendly_id :title, use: :slugged
 	DISQUS_SHORTNAME = Rails.env == "development" ? "dev_shortname".freeze : "production_shortname".freeze 
